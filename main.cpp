39f362ae7301783c2a20dde07204f84451a55383
Boris2006 <92259287+Boris2006@users.noreply.github.com>
2021-12-01 22:54:27 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,14 +1,16 @@
 #include "header.h"
 int main()
 {
-    string a, b;
-    char c;
+    string a, b, m;
+    int y;
+    long long e;
+    string c, d;
     cin >> a;
-    //b = oprst(a);
-    for (long long i = 0; a[i] != '-' && a[i] != '+'; i++){
-        c = a[i];
-        cout << trans(c);
-    }
-    //cout << b;
+    b = oprst(a);
+    c = first(a);
+    d = second(a);
+    e = len(a);
+    m = o(c, d);
+    cout << m;
     return 0;
 }
