63b4775e17aeaa79414838449be9bcea6d938bd3
Boris2006 <92259287+Boris2006@users.noreply.github.com>
2021-12-01 22:56:07 +0300

    Create trans2.cpp

---
+++ trans2.cpp
@@ -0,0 +1,24 @@
+#include "header.h"
+string trans2(int k){
+    string d;
+        if (k == 0){
+            d += "0";}
+        if (k == 1){
+            d += "1";}
+        if (k == 2){
+            d += "2";}
+        if (k == 3){
+            d += "3";}
+        if (k == 4){
+            d += "4";}
+        if (k == 5){
+            d += "5";}
+        if (k == 6){
+            d += "6";}
+        if (k == 7){
+            d += "7";}
+        if (k == 8){
+            d += "8";}
+        if (k == 9){
+            d += "9";}
+    return d;}
