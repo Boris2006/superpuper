d983244a7843c2fe5859e9c7def618003f8047b1
Boris2006 <92259287+Boris2006@users.noreply.github.com>
2021-12-01 18:20:59 +0300

    Delete super.h

--- super.h
+++
@@ -1,9 +0,0 @@
-#ifndef MIDDLE_H_INCLUDED
-#define MIDDLE_H_INCLUDED
-using namespace std;
-#include <string>
-#include <iostream>
-long long len(string a);
-long long  translation2(string e);
-bool examination(string a);
-#endif // MIDDLE_H_INCLUDED
