b898a7c4562741472138ade414a060f7babac35f
Boris2006 <92259287+Boris2006@users.noreply.github.com>
2021-12-01 23:03:39 +0300

    Update header.h

--- header.h
+++ header.h
@@ -8,4 +8,7 @@ long long trans(char a);
 string trans2(int k);
 string first( string a);
 string second(string a);
+string o(string c, string d);
+string o3(string c, string d);
+string o2(string c, string d);
 #endif // HEADER_H_INCLUDED
